9757902cd4a36a4b723bf0b07ffb055fd1dfa11a
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2016-06-04 20:04:39 -0500

    change to use mapzen
    more work needed.

--- geodata.py
+++ geodata.py
@@ -18,6 +18,8 @@ root_dir = os.path.dirname(os.path.realpath(__file__))
 # app = "newapp"
 # url = server_url+'/{}/{}/place?access_token={}'.format(org, app, token['access_token'])
 #
+mapzen_key = "search-jhsQPF4"
+mapzen = 'https://search.mapzen.com/v1/reverse?api_key={}'.format(mapzen_key)
 
 def buildings():
     with open(join(root_dir, 'geojson', 'austin_texas_buildings.geojson')) as f:
@@ -25,22 +27,24 @@ def buildings():
         for bldg in austin:
             pnt = list(shape(bldg['geometry']).centroid.coords[0])
             pnt.reverse()
-            loc = geocoder.osm(pnt, method="reverse").json
-            if 'osm_id' not in loc:
-                continue
-            loc['name'] = loc['osm_id']
-            fields_to_remove = ['encoding', 'status_code', 'confidence', 'provider', 'accuracy', 'status', 'importance',
-                                'icon', 'osm_type', 'place_rank', 'lat', 'lng']
-            yield {k: loc[k] for k in loc if k not in fields_to_remove}
+            loc = requests.get(mapzen+"&point.lat={}&point.lon={}".format(*pnt)).json()
+            yield loc
+            # loc = geocoder.osm(pnt, method="reverse").json
+            # if 'osm_id' not in loc:
+            #     print "Skipping: {}".format(loc)
+            #     continue
+            # loc['name'] = loc['osm_id']
+            # fields_to_remove = ['encoding', 'status_code', 'confidence', 'provider', 'accuracy', 'status', 'importance',
+            #                     'icon', 'osm_type', 'place_rank', 'lat', 'lng']
+            # yield {k: loc[k] for k in loc if k not in fields_to_remove}
 
-with open(join(root_dir, 'buildings.txt'), 'w+') as f:
-    for i, bldg in enumerate(buildings()):
-        f.write("{}\n".format(json.dumps(bldg)))
-        stdout.write("\r{} buildings.".format(i))
-        stdout.flush()
-    stdout.write('\n')
-    stdout.flush()
+# with open(join(root_dir, 'buildings.txt'), 'w+') as f:
+#     for i, bldg in enumerate(buildings()):
+#         f.write("{}\n".format(json.dumps(bldg)))
+#         stdout.write("\r{} buildings.".format(i))
+#         stdout.flush()
+#     stdout.write('\n')
+#     stdout.flush()
 
-# for i, bldg in enumerate(buildings()):
-#     r = requests.put(url, data=json.dumps(bldg))
-#     print "{}: {}".format(i, r)
+for i, bldg in enumerate(buildings()):
+    print json.dumps(bldg)
